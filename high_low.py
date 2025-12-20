efbbaaaf5dd3989665bddade7b5a9dd8929b89af
Tim Freestone <timothy.freestone@gmail.com>
2025-12-20 11:56:10 +1100

    Add final solution for problem without old code

--- high_low.py
+++ high_low.py
@@ -15,26 +15,3 @@ def high_and_low(numbers):
     for number in numbers.split(" "):
         new_list.append(int(number))
     return f"{sorted(new_list)[-1]} {sorted(new_list)[0]}"
-
-   
- 
-
-print(high_and_low(("7 12 -7 16 99 100 2 3 8 87")))
-
-
-
-
-
-
-    # numbers = list(numbers.replace(" ", ","))
-#    numbers = [x for x in numbers if x not in [","]]
-#     print(numbers)
-#     return numbers[-1], numbers[0]
-
-
-#  for i in numbers:
-#         if i.isdigit():
-#             new_list.append(i)
-    
-#     print(new_list)
-#     return new_list
