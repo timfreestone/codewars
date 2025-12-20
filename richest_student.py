6b63be4b1abe542002e071da2a634b1ef840bc99
Tim Freestone <timothy.freestone@gmail.com>
2025-12-21 09:09:48 +1100

    Add finalised solution to richest student problem

--- richest_student.py
+++ richest_student.py
@@ -1,3 +1,5 @@
+
+# Codewars - this class already existed
 class Student:
     def __init__(self, name, fives, tens, twenties):
         self.name = name
@@ -11,14 +13,21 @@ def most_money(students):
     student_money = {}
     for student in students:
         student_money[student.name] = (student.fives * 5 + student.tens * 10 + student.twenties * 20)
-    return student_money
+    max_value = max(student_money.values())
 
+    winners = [
+        name for name, value in student_money.items()
+        if value == max_value
+    ]
 
+    if len(winners) == 1:
+        return winners[0]
+    else:
+        return "all"
 
 students = [
     Student("Alice", 2, 1, 0),
-    Student("Bob", 0, 0, 2),
-    Student("Charlie", 4, 0, 0)
+    Student("Bob", 0, 0, 3),
+    Student("Charlie", 3, 0, 0),
+    Student("Tim", 0, 0, 2)
 ]
-
-print(most_money(students))
